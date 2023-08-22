29ea56ff6c06deb9ce8c263862b88812ad0434d0
Parikha Sharma <109408529+parikhasharma@users.noreply.github.com>
2023-08-22 22:35:57 +0530

    Update server.js

--- sharmaparikha-ME_QTRIPDYNAMIC/backend/server.js
+++ sharmaparikha-ME_QTRIPDYNAMIC/backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 8082;
+const PORT = process.env.PORT || 8082;
 
 /*
 [GET API] used in module 1 to fetch data for all cities
